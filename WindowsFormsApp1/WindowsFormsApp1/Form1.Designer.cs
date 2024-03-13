8915ef0a1f2f1ded83619e0ec397abd98977e12f
ChakSem <alaa.chakori.semmane@gmail.com>
2024-03-13 13:50:51 +0100

    Avancement sur la partie 2

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -1,4 +1,5 @@
-﻿namespace WindowsFormsApp1
+﻿
+ namespace WindowsFormsApp1
 {
     partial class Form1
     {
@@ -10,7 +11,7 @@
         /// <summary>
         /// Nettoyage des ressources utilisées.
         /// </summary>
-        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -30,75 +31,102 @@
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fichierToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.ouvrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.enregistrezToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.Paramètres = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
-            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fichierToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(678, 33);
+            this.menuStrip1.Size = new System.Drawing.Size(682, 28);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
-            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
             // 
             // fichierToolStripMenuItem
             // 
             this.fichierToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.ouvrirToolStripMenuItem,
-            this.enregistrezToolStripMenuItem});
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2,
+            this.toolStripSeparator1,
+            this.Paramètres});
             this.fichierToolStripMenuItem.Name = "fichierToolStripMenuItem";
-            this.fichierToolStripMenuItem.Size = new System.Drawing.Size(74, 29);
+            this.fichierToolStripMenuItem.Size = new System.Drawing.Size(64, 24);
             this.fichierToolStripMenuItem.Text = "Fichier";
             // 
-            // ouvrirToolStripMenuItem
+            // toolStripMenuItem1
             // 
-            this.ouvrirToolStripMenuItem.Name = "ouvrirToolStripMenuItem";
-            this.ouvrirToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
-            this.ouvrirToolStripMenuItem.Text = "Ouvrir";
-            this.ouvrirToolStripMenuItem.Click += new System.EventHandler(this.ouvrirToolStripMenuItem_Click);
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(201, 26);
+            this.toolStripMenuItem1.Text = "Ouvrir";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
             // 
-            // enregistrezToolStripMenuItem
+            // toolStripMenuItem2
             // 
-            this.enregistrezToolStripMenuItem.Name = "enregistrezToolStripMenuItem";
-            this.enregistrezToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
-            this.enregistrezToolStripMenuItem.Text = "Enregistrez";
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(201, 26);
+            this.toolStripMenuItem2.Text = "Enregistrer sous ...";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(198, 6);
+            // 
+            // Paramètres
+            // 
+            this.Paramètres.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem4,
+            this.toolStripMenuItem5});
+            this.Paramètres.Name = "Paramètres";
+            this.Paramètres.Size = new System.Drawing.Size(201, 26);
+            this.Paramètres.Text = "Paramètres";
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(194, 26);
+            this.toolStripMenuItem4.Text = "Changer Taille";
+            this.toolStripMenuItem4.Click += new System.EventHandler(this.toolStripMenuItem4_Click);
+            // 
+            // toolStripMenuItem5
+            // 
+            this.toolStripMenuItem5.Name = "toolStripMenuItem5";
+            this.toolStripMenuItem5.Size = new System.Drawing.Size(194, 26);
+            this.toolStripMenuItem5.Text = "Changer Couleur";
+            this.toolStripMenuItem5.Click += new System.EventHandler(this.toolStripMenuItem5_Click);
             // 
             // textBox1
             // 
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBox1.Location = new System.Drawing.Point(0, 33);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(10);
+            this.textBox1.Location = new System.Drawing.Point(0, 28);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(10, 10, 10, 50);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(678, 411);
+            this.textBox1.Size = new System.Drawing.Size(682, 425);
             this.textBox1.TabIndex = 1;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // openFileDialog1
-            // 
-            this.openFileDialog1.FileName = "openFileDialog1";
-            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
-            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(678, 444);
+            this.ClientSize = new System.Drawing.Size(682, 453);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.menuStrip1);
-            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Éditeur de fichier texte";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -110,10 +138,13 @@
 
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem fichierToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem ouvrirToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem enregistrezToolStripMenuItem;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem Paramètres;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem5;
     }
 }
 
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -3,61 +3,189 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System;
-using System.ComponentModel;
-using System.Diagnostics;
-using System.Drawing;
-using System.IO;
-using System.Security;
-using System.Windows.Forms;
-
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        private bool contenuModifie = false;
+
         public Form1()
         {
             InitializeComponent();
+            contenuModifie = false;
+
+            // Changement taille de police 
+            textBox1.Font = new Font(textBox1.Font.FontFamily, Properties.Settings.Default.taillePolice, FontStyle.Regular);
+
+            // Changement couleur police 
+            textBox1.ForeColor = ColorTranslator.FromHtml(Properties.Settings.Default.couleurPolice);
+
+            // Changement de taille
+            this.Width = Properties.Settings.Default.tailleX;
+            this.Height = Properties.Settings.Default.tailleY;
+
+            // Changement de position
+            this.StartPosition = FormStartPosition.Manual;
+            this.Location = new Point(Properties.Settings.Default.positionX, Properties.Settings.Default.positionY);
+            }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Vérifie si le contenu a été modifié avant de fermer la fenêtre
+            if (contenuModifie)
+            {
+                // Demande à l'utilisateur de confirmer la fermeture sans enregistrer
+                DialogResult result = MessageBox.Show("Le contenu a été modifié. Voulez-vous vraiment quitter sans enregistrer ?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+                if (result == DialogResult.No)
+                {
+                    e.Cancel = true; // Annule la fermeture de la fenêtre
+                }
+            }
+            // On modifie et sauvegarde les paramètres
+            Properties.Settings.Default.tailleX = this.Width;
+            Properties.Settings.Default.tailleY = this.Height;
+
+            if(this.Location.X > 0 & this.Location.X < Screen.PrimaryScreen.Bounds.Width)
+            {
+                Properties.Settings.Default.positionX = this.Location.X;
+            }
+            else if (this.Location.X < 0)
+            {
+                Properties.Settings.Default.positionX = 0;
+            }
+            else if (this.Location.X > Screen.PrimaryScreen.Bounds.Width)
+            {
+                Properties.Settings.Default.positionX = Screen.PrimaryScreen.Bounds.Width;
+            }
+
+            if (this.Location.Y > 0 & this.Location.Y < Screen.PrimaryScreen.Bounds.Height)
+            {
+                Properties.Settings.Default.positionY = this.Location.Y;
+            }
+            else if (this.Location.Y < 0)
+            {
+                Properties.Settings.Default.positionY = 0;
+            }
+            else if (this.Location.Y > Screen.PrimaryScreen.Bounds.Height)
+            {
+                Properties.Settings.Default.positionY = Screen.PrimaryScreen.Bounds.Height;
+            }
+
+            Properties.Settings.Default.Save();
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-        
-        }
-
-        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
-        {
-            
-        }
-
-        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
-        {
-            string file = openFileDialog1.FileName;
-            /*File.ReadAllLines(file);*/ //Lire toutes les lignes du fichier 
-            List<string> lines = new List<string>();
-            lines.AddRange(File.ReadAllLines(file));
-
-            /*Pour chaque ligne dans la liste de lignes*/
-            foreach (string line in lines)
+            // Le contenu a été modifié, ajoute une étoile au titre de la fenêtre
+            if (!contenuModifie)
             {
-                textBox1.Text += line + "\r\n";//
+                this.Text = this.Text + "*";
+                contenuModifie = true;
             }
-
-
-           
-
-
+            
         }
 
-        private void ouvrirToolStripMenuItem_Click(object sender, EventArgs e)
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
         {
-            openFileDialog1.ShowDialog();
-            
+            // Crée un OpenFileDialog
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            // Configure les propriétés de l'OpenFileDialog
+            openFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            openFileDialog1.Filter = "Fichiers texte (*.txt)|*.txt";
+            openFileDialog1.Title = "Ouvrir un fichier texte";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+
+            // Affiche la boîte de dialogue et attend que l'utilisateur choisisse un fichier
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFileName = openFileDialog1.FileName;
+                
+                // Lecture du fichier 
+                using (StreamReader Sr = File.OpenText(selectedFileName))
+                {
+                    string Ligne;
+                    textBox1.Clear();
+
+                    // On remplit la texte box
+                    while ((Ligne = Sr.ReadLine()) != null)
+                    {
+                        textBox1.AppendText(Ligne + "\r\n");
+                    }
+                }
+                this.Text = "Éditeur de fichier texte [ " + Path.GetFileName(selectedFileName) + " ]";
+                contenuModifie = false;
+            }
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            // Crée un OpenFileDialog
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            // Configure les propriétés de l'OpenFileDialog
+            saveFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            saveFileDialog1.Filter = "Fichiers texte (*.txt)|*.txt";
+            saveFileDialog1.FilterIndex = 1;
+            saveFileDialog1.Title = "Enregistrer sous ...";
+
+            // Affiche la boîte de dialogue et attend que l'utilisateur choisisse un fichier
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // Obtient le chemin du fichier sélectionné
+                string filePath = saveFileDialog1.FileName;
+
+                using (StreamWriter sw = new StreamWriter(filePath, false))
+                {
+                    sw.Write(textBox1.Text);
+                }
+            }
+            contenuModifie = false;
+
+            // Retire l'étoile du titre de la fenêtre
+            this.Text = this.Text.TrimEnd('*');
+        }
+
+        private void toolStripMenuItem4_Click(object sender, EventArgs e)
+        {
+            // Créer une boîte de dialogue pour choisir la police
+            FontDialog fontDialog = new FontDialog();
+
+            // Afficher la boîte de dialogue
+            if (fontDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Récupérer la taille de police choisie par l'utilisateur
+                float taillePolice = fontDialog.Font.Size;
+
+                // Appliquer la nouvelle taille de police au TextBox
+                textBox1.Font = new Font(textBox1.Font.FontFamily, taillePolice);
+                Properties.Settings.Default.taillePolice = taillePolice;
+            }
+        }
+
+        private void toolStripMenuItem5_Click(object sender, EventArgs e)
+        {
+            // Créer une boîte de dialogue pour choisir la couleur
+            ColorDialog colorDialog = new ColorDialog();
+
+            // Afficher la boîte de dialogue
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Récupérer la couleur choisie par l'utilisateur
+                Color couleur = colorDialog.Color;
+
+                // Appliquer la nouvelle couleur au texte du TextBox
+                textBox1.ForeColor = couleur;
+                Properties.Settings.Default.couleurPolice = ColorTranslator.ToHtml(couleur);
+            }
         }
     }
 }
