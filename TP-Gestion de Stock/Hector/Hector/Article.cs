99943caa04e096aff035597b6daeedb1c299c582
ChakSem <alaa.chakori.semmane@gmail.com>
2024-03-20 10:20:01 +0100

    Classe parseur + classe Article

---
+++ TP-Gestion de Stock/Hector/Hector/Article.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Hector
+{
+s
+
+    class Article
+    {
+        private string Description;
+        private string Reference;
+        private string Marque;
+        private string Famille;
+        private string SousFamille;
+        private double PrixHT;
+
+        /**
+        * Constructeur de la classe Article
+        * Entrées:
+        * - string description: la description de l'article
+        * - string reference: la référence de l'article
+        * - string marque: la marque de l'article
+        * - string famille: la famille de l'article
+        * - string sousFamille: la sous-famille de l'article
+        * - double prixHT: le prix hors taxe de l'article
+        * Sortie:
+        */
+        public Article(string description, string reference, string marque, string famille, string sousFamille, double prixHT)
+        {
+            this.Description = description;
+            this.Reference = reference;
+            this.Marque = marque;
+            this.Famille = famille;
+            this.SousFamille = sousFamille;
+            this.PrixHT = prixHT;
+        }
+
+        /* Accesseurs des attributs de la classe Article */
+        public string GetDescription() { return Description; }
+        public void SetDescription(string value) { Description = value; }
+        public string GetReference() { return Reference; }
+        public void SetReference(string value) { Reference = value; }
+        public string GetMarque() { return Marque; }
+        public void SetMarque(string value) { Marque = value; }
+        public string GetFamille() { return Famille; }
+        public void SetFamille(string value) { Famille = value; }
+        public string GetSousFamille() { return SousFamille; }
+        public void SetSousFamille(string value) { SousFamille = value; }
+        public double GetPrixHT() { return PrixHT; }
+        public void SetPrixHT(double value) { PrixHT = value; }
+
+        /**
+        * Méthode qui permet d'afficher les articles de la liste
+        * Entrée: 
+        * - List<Article> articles: la liste des articles
+        * Sortie: 
+        * - string: la liste des articles dans le terminal
+        */
+        public static string AfficherArticles(List<Article> articles)
+        {
+            string res = "";
+            foreach (Article article in articles)
+            {
+                res += article.GetDescription() + " " + article.GetReference() + " " + article.GetMarque() + " " + article.GetFamille() + " " + article.GetSousFamille() + " " + article.GetPrixHT() + "\n";
+            }
+            Console.WriteLine(res); // Affichage dans le terminal
+            return res;
+        }
+    }
+
+
+}
+
---
+++ TP-Gestion de Stock/Hector/Hector/Parser.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hector
+{
+    class Parser
+    {
+        /**
+        * Methode qui permet de parser un fichier csv
+        * Entree: - string path: le chemin du fichier csv
+        * Sortie: - List<Article>: la liste des articles
+        */
+        public static List<Article> Parse(string path)
+        {
+            /*On crée une liste d'articles*/
+            List<Article> articles = new List<Article>();
+            /*On lit le fichier ligne par ligne*/
+            string[] lines = System.IO.File.ReadAllLines(path);
+
+            for (int i = 1; i < lines.Length; i++)// On commence à 1 pour ne pas prendre en compte la première ligne
+            {
+                string[] values = lines[i].Split(';');
+                string description = values[0];
+                string reference = values[1];
+                string marque = values[2];
+                string famille = values[3];
+                string sousFamille = values[4];
+                double prixHT = double.Parse(values[5]);
+                bool exist = false;
+
+                //on verifie que l'article n'est pas deja dans la liste
+                foreach (Article article in articles)
+                {
+                    if (article.GetReference() == reference)
+                    {
+                        exist = true;
+                    }
+                }
+                if (!exist)
+                    articles.Add(new Article(description, reference, marque, famille, sousFamille, prixHT));
+
+            }
+            return articles;
+        }
+    }
+}
+
