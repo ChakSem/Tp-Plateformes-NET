d0870946426327c40eb969b4fddb8d820442c562
ChakSem <alaa.chakori.semmane@gmail.com>
2024-03-27 11:13:45 +0100

    commentaires

--- TP-Gestion de Stock/Hector/Hector/FormMain.cs
+++ TP-Gestion de Stock/Hector/Hector/FormMain.cs
@@ -8,24 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-// Dansvotre TreeView(Partie gauche) vous ajouterez plusieurs éléments qui permettront de charger votre ListView(Partie
-// droite) selon certains critères.
-//  • Nœudsracines(Nœudsdepremierniveau) :
-//  • Nœud«Touslesarticles »: Chargera la liste complète des articles dans le ListView.
-//  • Nœud«Familles»:Chargerala liste des familles dans le ListView.
-//  • Nœud«Marques»:Chargeralaliste des marques dans le ListView.
-//  • Lenœud«Familles»devradisposer de la liste complète des familles » en tant que nœuds enfants, et chaque nœud enfant
-//  « famille » devra disposer des « sous-familles ».
-//  • Unclicsurunnœud«famille» chargera la liste des « sous-familles » dans le ListView.
-//  • Unclic sur un nœud « sous-famille » chargera la liste des « articles » appartenant à cette sous-famille dans le
-//  • Unclic sur un nœud « sous-famille » chargera la liste des « articles » appartenant à cette sous-famille dans le
-//  ListView.
-//  • Lenœud«Marques»devraavoirennœudsenfantslaliste complète des marques.
-//  • Unclicsurunnœud«marque»chargeralaliste des « articles » appartenantà cette marque dans le ListView.
-//  • Danschacundescas, vous devrez gérer le trie des données à l’écran (exclusivement dans l’objet « ListView
-//  », n’utilisez pas
-//  de requête) via un clic sur les entêtes de colonne.Cette option devra permettre de trier de manière ascendante et
-//  descendante et je le répète : sans recharger les données.
+
 namespace Hector
 {
     public partial class FormMain : Form
